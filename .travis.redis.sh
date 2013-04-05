5f03b3cb3c9f2b9bf158528c0b9ae1958b0dbb98
Filip Procházka <hosiplan@gmail.com>
2013-04-06 00:17:23 +0200

    Travis: build agains master of phpredis

--- .travis.redis.sh
+++ .travis.redis.sh
@@ -2,7 +2,7 @@
 
 mkdir build && cd build || exit 1
 
-wget -O phpredis.zip "https://github.com/nicolasff/phpredis/archive/2.2.2.zip" && unzip phpredis.zip && cd phpredis-2.2.2/ || exit 1
+wget -O phpredis.zip "https://github.com/nicolasff/phpredis/archive/master.zip" && unzip phpredis.zip && cd phpredis-master/ || exit 1
 echo
 
 phpize && ./configure && make && sudo make install || exit 1
