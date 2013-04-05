a1cf9d31cb02eabef06991af5d46fd40854ee284
Filip Procházka <hosiplan@gmail.com>
2013-04-06 00:06:32 +0200

    Travis: build 2.2.2 version of phpredis

--- .travis.redis.sh
+++ .travis.redis.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 mkdir build && cd build || exit 1
-git clone --depth 1 git://github.com/nicolasff/phpredis.git && cd phpredis || exit 1
-phpize && ./configure && make && sudo make install && cd .. || exit 1
+
+wget -O phpredis.zip "https://github.com/nicolasff/phpredis/archive/2.2.2.zip" && unzip phpredis.zip && cd phpredis-2.2.2/ || exit 1
+echo
+
+phpize && ./configure && make && sudo make install || exit 1
+echo
 
 echo 'extension = redis.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
