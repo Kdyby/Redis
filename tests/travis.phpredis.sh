ecbfd4d4fef166949e26a64e3221431ec4b00052
Filip Procházka <filip@prochazka.su>
2016-07-09 00:25:31 +0200

    Travis: update redis using apt addon

--- tests/travis.phpredis.sh
+++ tests/travis.phpredis.sh
@@ -5,7 +5,7 @@ mkdir ~/build-phpredis && cd ~/build-phpredis || exit 1
 wget -O phpredis.zip "https://github.com/nicolasff/phpredis/archive/master.zip" && unzip phpredis.zip && cd phpredis-master/ || exit 1
 echo
 
-phpize && ./configure && make && sudo make install || exit 1
+phpize && ./configure && make && make install || exit 1
 echo
 
 echo 'extension = redis.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
