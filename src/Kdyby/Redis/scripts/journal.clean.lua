821c6a5a7db1fc1122dec375dd132377fa4f6599
Jakub Vojacek <jakub@jacon.cz>
2021-02-11 09:02:09 +0100

    Improved clean.lua

--- src/Kdyby/Redis/scripts/journal.clean.lua
+++ src/Kdyby/Redis/scripts/journal.clean.lua
@@ -1,28 +1,40 @@
+local formatKey = function (key, suffix)
+    local res = "Nette.Journal:" .. key
+    if suffix ~= nil then
+        res = res .. ":" .. suffix
+    end
+
+    return res
+end
+
+local entryTags = function (key)
+    return redis.call('sMembers', formatKey(key, "tags"))
+end
+
+local tagEntries = function (tag)
+    return redis.call('sMembers', formatKey(tag, "keys"))
+end
 
 local conds = cjson.decode(ARGV[1])
 
-if conds["all"] ~= nil then
-    -- redis.call('multi')
-    for i, value in pairs(redis.call('keys', "Nette.Journal:*")) do
-        redis.call('del', value)
-    end
-    -- redis.call('exec')
-
-    return redis.status_reply("Ok")
-end
-
-local entries = {}
 if conds["tags"] ~= nil then
     for i, tag in pairs(conds["tags"]) do
         local found = tagEntries(tag)
-        if #found > 0 then
-            cleanEntry(found)
 
+        if #found > 0 then
             for i, key in pairs(found) do
+                local tags = entryTags(key)
+                -- redis.call('multi')
+                for i, tag in pairs(tags) do
+                    redis.call('sRem', formatKey(tag, "keys"), key)
+                end
+                -- drop tags of entry and priority, in case there are some
+                redis.call('del', formatKey(key, "tags"))
+
                 redis.call("del", key)
             end
         end
     end
 end
 
-return entries
+return redis.status_reply("Ok")
